a57d0b7272735fe68eeefcd7093e62a793479613
Andi Buist <105498051+andi-buist@users.noreply.github.com>
2026-09-15 23:14:32 +0100

    converted language to a LangParticle class, added a new decorator to preload certain named brushstrokes into constants

--- src/glyphs.py
+++ src/glyphs.py
@@ -1,11 +1,47 @@
 from PIL import Image, ImageDraw
+from typing import Literal
 
-# TODO: potentially add named constructors? e.g. BrushStroke('topbar') -> [(1,1),(1,7)]
+GLYPH_MARGIN = 0.1
+
+def add_constants(cls):
+    HOR = [(0.0, 0.0), (1.0, 0.0)]
+    VER = [(0.0, 0.0), (0.0, 1.0)]
+
+    _offsets = [0.2, 0.4, 0.6, 0.8]
+
+    for offset in _offsets:
+        _hor_path = [(x, y + offset) for (x,y) in HOR]
+        _ver_path = [(x + offset, y) for (x,y) in VER]
+
+        _hor_constant_name = f"HOR_0{int(offset * 10)}"
+        _ver_constant_name = f"VER_0{int(offset * 10)}"
+
+        setattr(cls, _hor_constant_name, cls(path = _hor_path))
+        setattr(cls, _ver_constant_name, cls(path = _ver_path))
+
+    cls.TOP = cls(path = HOR)
+    cls.HOR_CENTRE = cls(path=[(0.0, 0.5), (1.0, 0.5)])
+    cls.BOTTOM = cls(path=[(0.0, 1.0), (1.0, 1.0)])
+
+    cls.LEFT = cls(path = VER)
+    cls.VER_CENTRE = cls(path=[(0.5, 0.0), (0.5, 1.0)])
+    cls.RIGHT = cls(path=[(1.0, 0.0), (1.0, 1.0)])
+
+    cls.LEG_LEFT = cls(path=[(0.1, 0.0), (0.1, 0.7), (0.0, 1.0)], weight = 2)
+
+    cls.CIRCLE = cls(path=[(0.5, 0.0), (0.0, 0.0), (0.0, 0.5), (0.0, 1.0), (0.5, 1.0), (1.0, 1.0), (1.0, 0.5), (1.0, 0.0), (0.5, 0.0)], weight = 1)
+
+    return cls
+
+@add_constants 
 class BrushStroke():
-    def __init__(self, path: list[tuple[int,int]], thickness: int = 6.0, weight: int | list[int] = 3):
+    def __init__(self, path: list[tuple[float, float]], thickness: float = 6.0, weight: int | list[int] = 4):
         if type(weight) is list:
             if len(weight) != len(path):
                 raise ValueError('A list of weights must be the same length as the path list')
+
+        if any(not 0.0 <= value <= 1.0 for point in path for value in point):
+            raise ValueError('Brush stroke coordinates must be between 0.0 and 1.0')
 
         self.path = path
         self.thickness = thickness
@@ -50,11 +86,8 @@ class BrushStroke():
     def draw(self, canvas: Image):
         _active_canvas = canvas
 
-        if not hasattr(_active_canvas, "margin"): raise AttributeError('canvas does not have attribute "margin"')
-        if not hasattr(_active_canvas, "subdivision"): raise AttributeError('canvas does not have attribute "subdivision"')
-
-        _canvas_area = tuple(x - _active_canvas.margin * 2 for x in _active_canvas.size)
-        _canvas_fraction = tuple(x/_active_canvas.subdivision for x in _canvas_area)
+        _canvas_area = tuple(x * (1 - GLYPH_MARGIN * 2) for x in _active_canvas.size)
+        _canvas_margin = tuple(x * GLYPH_MARGIN for x in _active_canvas.size)
 
         # repeat coords to reduce bezier smoothing; _weighting = level of smoothness
         _weighted_path = []
@@ -67,7 +100,7 @@ class BrushStroke():
         _scaled_path = []
 
         for x in _weighted_path:
-            _pos = tuple((_active_canvas.margin + (x[n] * _canvas_fraction[n]) - (_canvas_fraction[n]/2)) for n in [0,1])
+            _pos = tuple(_canvas_margin[n] + x[n] * _canvas_area[n] for n in [0, 1])
             _scaled_path.append(_pos)
 
         _draw = ImageDraw.Draw(_active_canvas)
@@ -86,20 +119,31 @@ class BrushStroke():
                         fill=(0,0,0,255))
         return _active_canvas
 
-# TODO: classify, convert canvas generation and BrushStroke.draw() calls to a Glyph.draw() call. Add defs, pronunciation, etc. Also fromJSON constructor?
-def DrawCharacter(strokes: list[BrushStroke]):
-    """
-    Constructs a PIL.Image from a list of brush strokes.
+class LangParticle():
+    PARTICLE_TYPES = Literal["noun", "verb", "adjective", "adverb", "pronoun", "preposition", "conjunction", "number"]
 
-    :param strokes: A list of `BrushStroke`s. These define the 'cells' that the 'brush' passes through during a single line stroke.
-    :type strokes: list[BrushStroke]
+    def __init__(self, particle_type: PARTICLE_TYPES, definition: str | int, strokes: BrushStroke | list[BrushStroke]):
+        if any((particle_type == "number" and type(definition) is not int, particle_type != "number" and type(definition) is int)):
+            raise ValueError("LangParticle `particle_type` and `definition` are incompatible!")
 
-    """
-    _active_canvas = Image.new(mode = "RGBA", size = (128,128), color = (255,255,255,255))
-    _active_canvas.margin = 1
-    _active_canvas.subdivision = 7
+        self.glyph = self._generate_character(strokes)
+        self.particle_type = particle_type
+        self.definition = definition
 
-    for stroke in strokes:
-        _active_canvas = stroke.draw(_active_canvas)
+    def _generate_character(self, strokes: list[BrushStroke]):
+        """
+        Constructs a PIL.Image from a list of brush strokes.
 
-    return _active_canvas
+        :param strokes: A list of `BrushStroke`s. These define the 'cells' that the 'brush' passes through during a single line stroke.
+        :type strokes: list[BrushStroke]
+
+        """
+        _active_canvas = Image.new(mode = "RGBA", size = (128,128), color = (255,255,255,255))
+
+        if type(strokes) is list:
+            for stroke in strokes:
+                _active_canvas = stroke.draw(_active_canvas)
+        else:
+            _active_canvas = strokes.draw(_active_canvas)
+
+        return _active_canvas
